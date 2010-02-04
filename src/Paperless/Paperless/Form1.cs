a6f2fa81b6758402d591bdb5486b416b409c85b9
Jon Fuller <fullerjc@gmail.com>
2010-02-03 21:27:37 -0500

    add spike for mongo communication

--- src/Paperless/Paperless/Form1.cs
+++ src/Paperless/Paperless/Form1.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MongoDB.Driver;
+
+// date needs to be defaulted to today, but changeable.
 
 namespace Paperless
 {
@@ -14,6 +18,46 @@ namespace Paperless
         public Form1()
         {
             InitializeComponent();
+
+            Whatever();
+        }
+
+        private void Whatever()
+        {
+            var mongo = new Mongo();
+            mongo.Connect();
+            var db = mongo.getDB("Paperless");
+            var collection = db.GetCollection("docs");
+
+            collection.Insert(new Document()
+                                      {
+                                          {"tags", new[]{"bill", "sandi", "receipt", "2010"}},
+                                          {"date", DateTime.Now},
+                                          {"original_filename", "check it out.pdf"},
+                                          {"content", new Binary( GetPdfContent(@"C:\Users\jon\Dev\Paperless\test\1page.pdf"))}
+                                      });
+        
+        }
+
+        private byte[] GetPdfContent(string filename)
+        {
+            using (var file = new FileStream(filename, FileMode.Open))
+            using (var reader = new BinaryReader(file))
+            {
+                var content = new List<byte>();
+
+                byte[] readBuffer = new byte[4096];
+                int lengthRead;
+
+                do
+                {
+                    lengthRead = reader.Read(readBuffer, 0, readBuffer.Length);
+
+                    content.AddRange(readBuffer.ToList().GetRange(0, lengthRead));
+                } while (lengthRead > 0);
+
+                return content.ToArray();
+            }
         }
     }
 }
